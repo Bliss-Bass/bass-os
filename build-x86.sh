fdf6bd0f7886d2542db331290fa27fc7a40c9c01
electrikjesus <electrikjesus@gmail.com>
2024-03-10 14:05:47 -0400

    scripts: fix script wrapper headings and feedback

--- build-x86.sh
+++ build-x86.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-trap 'echo -e "\nbuild.sh interrupted"; exit 1' SIGINT
+trap 'echo -e "\nbuild-x86.sh interrupted"; exit 1' SIGINT
 
-echo Building the Android
+echo -e "\033[1;34mBuilding Bass OS\033[0m"
 pushd aosptree
 . build/envsetup.sh
 build-x86 $@
