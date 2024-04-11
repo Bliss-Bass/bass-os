db67019e61e8b11d3211423a6916806012244e49
Jon West <electrikjesus@gmail.com>
2024-04-11 19:01:38 -0400

    scripts: update signing key generation scope

--- aosptree/vendor/bass/tools/tool-generate-keys.sh
+++ aosptree/vendor/bass/tools/tool-generate-keys.sh
@@ -7,6 +7,10 @@
 
 subject='/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN=Android/emailAddress=contact@blisslabs.org'
 mkdir vendor/bliss/config/signing
-for x in testkey releasekey platform shared media networkstack; do \
+for x in testkey releasekey platform shared media sdk_sandbox bluetooth networkstack fullchain; do \
   ./development/tools/make_key vendor/bliss/config/signing/$x "$subject"; \
 done
+
+openssl genrsa > vendor/bliss/config/signing/privkey.pem
+openssl req -new -x509 -key vendor/bliss/config/signing/privkey.pem > vendor/bliss/config/signing/fullchain.pem
+openssl req -new -x509 -key vendor/bliss/config/signing/privkey.pem > vendor/bliss/config/signing/cert.pem
