98ade018ea5b13322d59564e644df5455f743c7d
Jon West <electrikjesus@gmail.com>
2024-05-23 17:40:52 -0400

    scripts: copy any vendor logos found into build
    any logs or media files found in templates/product/media will be copied to the device product/media folder

--- aosptree/vendor/bass/branding.mk
+++ aosptree/vendor/bass/branding.mk
@@ -197,3 +197,7 @@ $(foreach f,$(wildcard $(LOCAL_PATH)/templates/vendor/etc/*.xml),\
 
 endif
 
+# Copy any product specific input configs if found
+$(foreach f,$(wildcard $(LOCAL_PATH)/templates/product/media/*),\
+    $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_PRODUCT)/media/$(notdir $f)))
+
