9482cb7069afebc967279fec6acdd4f69ff7b151
Jon West <electrikjesus@gmail.com>
2024-08-02 10:12:47 -0400

    [bass] Add vendor sku properties

--- aosptree/vendor/bass/branding.mk
+++ aosptree/vendor/bass/branding.mk
@@ -208,3 +208,8 @@ endif
 $(foreach f,$(wildcard $(LOCAL_PATH)/templates/product/media/*),\
     $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_PRODUCT)/media/$(notdir $f)))
 
+# Vendor unique build identifier
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.bliss.device.vendor.id=BASS.DEMO.A01-001 \
+    ro.boot.hardware.sku=BASS.DEMO.A01-001 \
+    ro.boot.product.hardware.sku=BASS.DEMO.A01-001
