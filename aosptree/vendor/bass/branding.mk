e6d52b0abd5efaad550e3c35177c0bebe8d6f9dc
Jon West <electrikjesus@gmail.com>
2024-08-13 15:23:00 -0400

    bass: add property for licensing
    ro.bliss.device.is.licensed to be set to false by default

--- aosptree/vendor/bass/branding.mk
+++ aosptree/vendor/bass/branding.mk
@@ -212,4 +212,5 @@ $(foreach f,$(wildcard $(LOCAL_PATH)/templates/product/media/*),\
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.bliss.device.vendor.id=BASS.DEMO.A01-001 \
     ro.boot.hardware.sku=BASS.DEMO.A01-001 \
-    ro.boot.product.hardware.sku=BASS.DEMO.A01-001
+    ro.boot.product.hardware.sku=BASS.DEMO.A01-001 \
+    ro.bliss.device.is.licensed=false
