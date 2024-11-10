41f0eca285818d8718ba1b04b18d46ca868cd665
Jon West <electrikjesus@gmail.com>
2024-11-10 16:19:12 -0500

    bass: update vncflinger inclusion

--- aosptree/vendor/bass/branding.mk
+++ aosptree/vendor/bass/branding.mk
@@ -218,6 +218,11 @@ PRODUCT_PACKAGES += \
     DesktopMode \
     VncFlinger
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.software.freeform_window_management.xml
+
+$(call inherit-product-if-exists, packages/services/VncFlinger/product.mk)
+
 endif
 
 # Copy any Permissions files, overriding anything if needed
